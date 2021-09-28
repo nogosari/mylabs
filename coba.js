a62f6c6a0da29377226708c5158f72a50e661180
nogosari <littlebadboy1945@gmail.com>
2021-09-28 15:29:39 +0700

    update

---
+++ coba.js
@@ -0,0 +1,10 @@
+const getUserSync = require(./src/getUserSync);
+
+const userA = getUserSync(1);
+console.log(userA);
+
+const userB = getUserSync(2);
+console.log(userB);
+
+const halo = 'Hello World';
+console.log(halo);
